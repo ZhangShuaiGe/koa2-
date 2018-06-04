608254d2a1e8bccfa2964addfbac15bb1f425b36
张帅 <zhangshuai@beecredit.com>
2018-06-04 18:41:55 +0800

    mysql修改

--- controller/mysql.js
+++ controller/mysql.js
@@ -1,5 +1,5 @@
-var mysql  = require('mysql');
-var connection = mysql.createConnection({
+const mysql  = require('mysql');
+const pool = mysql.createPool({
     host     : 'localhost',
     user     : 'root',
     port     : 3307,
@@ -7,12 +7,54 @@ var connection = mysql.createConnection({
     database : 'web'
 });
 
-connection.connect(function (err) {
-    if (err) {
-        global.info_logger.info("mysql 连接失败：" + err.stack);
-        return;
-    }
-    global.info_logger.info("mysql 连接成功");
-});
+// pool.connect(function (err) {
+//     if (err) {
+//         global.info_logger.info("mysql 连接失败：" + err.stack);
+//         return;
+//     }
+//     global.info_logger.info("mysql 连接成功");
+// });
 
-module.exports = connection;
+const query = ( sql, values ) => {
+
+    return new Promise(( resolve, reject ) => {
+        pool.getConnection( (err, connection) => {
+            if (err) {
+                reject( err )
+            } else {
+                connection.query(sql, values, ( err, rows) => {
+                    if ( err ) {
+                        reject( err )
+                    } else {
+                        resolve( rows )
+                    }
+                    connection.release()
+                })
+            }
+        })
+    })
+
+};
+
+// const query = (sql,data) => {
+//     return new Promise(resolve,reject) => {
+//         pool.getConnection((err,connection)=>{
+//             connection.query(sql,data,function (err,results,fields) {
+//                 if(err){
+//                     console.log("sql报错：" + err);
+//                     reject(err);
+//                 }
+//                 if(results){
+//                     resolve(results);
+//                 }
+//                 connection.release();
+//             });
+//         })
+//     }
+// };
+
+// 注册
+exports.register = function (val) {
+    let sql = "insert into user(USERNAME,PASSWORD) values(?,?);";
+    return query(sql,val);
+};
--- controller/web.js
+++ controller/web.js
@@ -1,4 +1,4 @@
-const connection = require("./mysql");
+const mysql = require("./mysql");
 exports.index = (ctx) => {
     ctx.render("index");
 };
@@ -8,57 +8,14 @@ exports.login = (ctx) => {
 };
 
 exports.register = async (ctx) => {
-    console.log(111111);
-    var xhr = ctx.get("x-requested-with");
-    if(xhr){
-        var a = 100;
-        await setTimeout(function () {
-            a = 1000;
-        },3000);
-        console.log(a);
-        // console.log(ctx.request.body);
-        // // post请求
-        // var sql = "insert into user(USERNAME,PASSWORD) values(?,?);";
-        // var data = [ctx.request.body.username,ctx.request.body.password];
-        // console.log(2222222222);
-        // let result = await connection.query(sql,data,function (err,results,fields) {
-        //     if(err){
-        //         console.log("sql报错：" + err);
-        //     }
-        //     if(results){
-        //         console.log(33333333);
-        //         return 123123;
-        //         // console.log(ctx);
-        //         // ctx.body = {
-        //         //     a:"我的天121212",
-        //         //     b:123
-        //         // }
-        //         // return ctx.request.body = {
-        //         //    code:1
-        //         // };
-        //         // console.log("成功");
-        //         // res.redirect("/login");
-        //     }
-        // });
-        //
-        // console.log(444444444);
-        // console.log(result);
-        // console.log(ctx.body);
-        // console.log(1111111111);
-        // ctx.body = {
-        //     a:"我的天121212",
-        //     b:123
-        // }
-    }else{
+    if(ctx.method == "POST"){
+        console.log(111111111);
+        var a = mysql.register([ctx.request.body.username,ctx.request.body.password]);
+        a.then(function (data) {
+            console.log(data);
+        });
+    } else {
         // get请求
         ctx.render("user/register");
-    }
-};
-
-exports.test = async (ctx) => {
-    console.log("进入");
-    ctx.body = {
-        a:123,
-        b:123
     }
 };
