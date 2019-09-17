abec897735d2a65466d58183a87d327f8b35d0e4
张帅 <zhangshuai@beecredit.com>
2019-09-17 18:48:46 +0800

    gulp配置

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,56 @@
-var gulp = require('gulp');
+const {task, dest, src, series} = require('gulp');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const del = require('del');
 
-gulp.task('default', function() {
-    // 将你的默认的任务代码放在这
+const config = {
+    // 入口
+    entryPath: {
+        static: "static/**/*",
+        js: "static/**/*.js",
+        css: "static/**/*.css",
+        html: "views/**/*.html",
+    },
+    // 出口
+    outPath: {
+        static: "distStatic/",
+        views: "distViews/",
+    },
+    // 忽略构建的文件
+    ignore: {
+        js: [
+            "!static/adminStatic/**/*.js",
+            "!static/lib/**/*.js"
+        ]
+    }
+};
+
+// 克隆static
+// task("cloneStatic", function () {
+//     return src(config.entryPath.static)
+//             .pipe(dest(config.outPath.static))
+// });
+
+// 克隆views
+// task("cloneViews", function () {
+//     return src("static/**/*")
+//             .pipe(dest(config.outPath.views))
+// });
+
+task("js", function () {
+    return src([config.entryPath.js,...config.ignore.js])
+            .pipe(babel({
+                presets: ['@babel/env']
+            }))
+            .pipe(uglify())
+            .pipe(dest(config.outPath.static))
 });
+
+//清除目录
+task("clear", async function () {
+    let directory = Object.values(config.outPath);
+    return await del(directory);
+});
+
+// task("default", series("clear","cloneStatic", "cloneViews"));
+task("default", series("clear", "js"));
